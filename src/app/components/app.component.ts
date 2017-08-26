f92cc60bff10dba6e4b5ed2d652c154f3ba6e57d
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-26 11:58:39 -0600

    used the session API on init

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,8 +1,16 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {SessionService} from "../services/session.service";
 
 @Component({
 	selector: "angular4-webpack-devserver",
 	templateUrl: "./templates/angular4-webpack-devserver.html"
 })
 
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+	constructor(protected sessionService: SessionService) {}
+
+	ngOnInit() : void {
+		this.sessionService.setSession();
+	}
+}
