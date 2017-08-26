80119647fdfedd40cee47a40315b1005b0d28801
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-26 12:16:09 -0600

    updated user component to call user service

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,7 +1,23 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {UserService} from "../services/user.service";
+import {User} from "../classes/user";
 
 @Component({
 	templateUrl: "./templates/user.html"
 })
 
-export class UserComponent {}
+export class UserComponent implements OnInit {
+
+	users : User[] = [];
+
+	constructor(protected userService: UserService) {}
+
+	ngOnInit() : void {
+		this.getAllUsers();
+	}
+
+	getAllUsers() : void {
+		this.userService.getAllUsers()
+			.subscribe(users => this.users = users);
+	}
+}
