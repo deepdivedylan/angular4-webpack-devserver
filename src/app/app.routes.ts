6ded33e355a895b9b49c4d5073b986ab458d67da
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-26 12:10:27 -0600

    added user service to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {UserComponent} from "./components/user.component";
 import {SessionService} from "./services/session.service";
+import {UserService} from "./services/user.service";
 
 
 export const allAppComponents = [HomeComponent, UserComponent];
@@ -11,6 +12,6 @@ export const routes: Routes = [
 	{path: "", component: HomeComponent}
 ];
 
-export const appRoutingProviders: any[] = [SessionService];
+export const appRoutingProviders: any[] = [SessionService, UserService];
 
 export const routing = RouterModule.forRoot(routes);
