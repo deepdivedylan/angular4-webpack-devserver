530b3dfba062264446f56b1d70bdf1eb48f9d31d
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-26 12:09:25 -0600

    added user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
+import {User} from "../classes/user";
+
+@Injectable()
+export class UserService extends BaseService {
+
+	constructor(protected http : Http) {
+		super(http);
+	}
+
+	private userUrl = "https://jsonplaceholder.typicode.com/users/";
+
+	getAllUsers() : Observable<User[]> {
+		return(this.http.get(this.userUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getUserByUserId(userId : number) : Observable<User> {
+		return(this.http.get(this.userUrl + userId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+}
